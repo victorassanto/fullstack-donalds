0489030ee0f0c3416940d072e2832a54d916bf41
victorassanto <victor-159753@hotmail.com>
2025-02-22 13:38:53 -0300

    feat: add orders page

--- src/app/[slug]/menu/actions/create-order.ts
+++ src/app/[slug]/menu/actions/create-order.ts
@@ -1,6 +1,7 @@
 "use server";
 
 import { ConsumptionMethod } from "@prisma/client";
+import { redirect } from "next/navigation";
 
 import { db } from "@/lib/prisma";
 
@@ -56,5 +57,5 @@ export const createOrder = async (input: CreateOrderInput) => {
       restaurantId: restaurant.id,
     },
   });
-  
+  redirect(`/${input.slug}/orders`);
 };
